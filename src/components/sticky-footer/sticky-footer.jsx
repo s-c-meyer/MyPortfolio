38b4bf837bb8dbca5fb3b7dad396895f07e32a35
s-c-meyer <meyer.samuelc@gmail.com>
2024-06-14 11:04:36 -0500

    Fixed Sizing and Spacing across the site
    Also removed LinkedIn Public Profile Badge, as the script was blocked on FireFox. Fixed spacing on portfolio page, and font size on index page

--- src/components/sticky-footer/sticky-footer.jsx
+++ src/components/sticky-footer/sticky-footer.jsx
@@ -7,8 +7,8 @@ export const StickyFooter = () => {
     <>
       <Navbar className="sticky-footer" fixed="bottom" bg="light" variant="light">
         <Container className="d-flex flex-row justify-content-center align-items-center">
-          <p className="mb-0 me-3">Find me at any of these links: </p>
-          <a href="https://github.com/s-c-meyer?tab=projects" target="_blank"><img className="footer-image" src={require("../../img/github_icon.svg")} alt="Github icon" /></a>
+          <p xs={12} className="mb-0 me-3">Find me at any of these links: </p>
+          <a href="https://github.com/s-c-meyer" target="_blank"><img className="footer-image" src={require("../../img/github_icon.svg")} alt="Github icon" /></a>
           <a href="https://www.facebook.com/sam.c.meyer/" target="_blank"><img className="footer-image" src={require("../../img/fb_icon.svg")} alt="Facebook icon" /></a>
           <a href="https://www.linkedin.com/in/samuel-christopher-meyer/" target="_blank"><img className="footer-image" src={require("../../img/linkedin_icon.svg")} alt="LinkedIn icon" /></a>
         </Container>
